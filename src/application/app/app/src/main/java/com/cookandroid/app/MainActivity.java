3fa38f073d0e5f45ed2ed47d2c7cce0dc1de0cf4
Jonggun Park <ooostus@gmail.com>
2022-04-06 18:56:18 +0900

    dummy commit

--- src/application/app/app/src/main/java/com/cookandroid/app/MainActivity.java
+++ src/application/app/app/src/main/java/com/cookandroid/app/MainActivity.java
@@ -8,11 +8,11 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.telephony.SmsManager;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -21,12 +21,31 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import android.telephony.SmsManager;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     EditText edit;
     Button button;
 
-    static final int SMS_RECEIVE_PERMISSON=1;
+    static final int SMS_SEND_PERMISSON = 1; // SMS 송신 권한 설정용 변수
+
+
+    private static final String CLOUD_VISION_API_KEY = BuildConfig.API_KEY;
+    public static final String FILE_NAME = "temp.jpg";
+    private static final String ANDROID_CERT_HEADER = "X-Android-Cert";
+    private static final String ANDROID_PACKAGE_HEADER = "X-Android-Package";
+    private static final int MAX_LABEL_RESULTS = 10;
+    private static final int MAX_DIMENSION = 1200;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int GALLERY_PERMISSIONS_REQUEST = 0;
+    private static final int GALLERY_IMAGE_REQUEST = 1;
+    public static final int CAMERA_PERMISSIONS_REQUEST = 2;
+    public static final int CAMERA_IMAGE_REQUEST = 3;
+
+    private TextView mImageDetails;
+    private ImageView mMainImage;
 
 
     @Override
@@ -34,18 +53,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        // SMS 송신 권한 설정
         int permissonCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS);
+
         if(permissonCheck == PackageManager.PERMISSION_GRANTED){
-            Toast.makeText(getApplicationContext(), "SMS 수신권한 있음", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "SMS 송신 권한 있음", Toast.LENGTH_SHORT).show();
         }else{
-            Toast.makeText(getApplicationContext(), "SMS 수신권한 없음", Toast.LENGTH_SHORT).show();
-        //권한설정 dialog에서 거부를 누르면 //ActivityCompat.shouldShowRequestPermissionRationale 메소드의 반환값이 true가 된다. //단, 사용자가 "Don't ask again"을 체크한 경우 //거부하더라도 false를 반환하여, 직접 사용자가 권한을 부여하지 않는 이상, 권한을 요청할 수 없게 된다.
+            Toast.makeText(getApplicationContext(), "SMS 송신 권한 없음", Toast.LENGTH_SHORT).show();
+            // 권한설정 dialog에서 거부를 누르면
+            // ActivityCompat.shouldShowRequestPermissionRationale 메소드의 반환값이 true가 된다.
+            // 단, 사용자가 "Don't ask again"을 체크한 경우
+            // 거부하더라도 false를 반환하여, 직접 사용자가 권한을 부여하지 않는 이상, 권한을 요청할 수 없게 된다.
             if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS)){
-                //이곳에 권한이 왜 필요한지 설명하는 Toast나 dialog를 띄워준 후, 다시 권한을 요청한다.
+                // 이곳에 권한이 왜 필요한지 설명하는 Toast나 dialog를 띄워준 후, 다시 권한을 요청한다.
                 Toast.makeText(getApplicationContext(), "SMS권한이 필요합니다", Toast.LENGTH_SHORT).show();
-                ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.SEND_SMS}, SMS_RECEIVE_PERMISSON);
+                ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.SEND_SMS}, SMS_SEND_PERMISSON);
             }else{
-                ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.SEND_SMS}, SMS_RECEIVE_PERMISSON);
+                ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.SEND_SMS}, SMS_SEND_PERMISSON);
             }
         }
 
@@ -58,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Toast toast = Toast.makeText(getApplicationContext(), edit.getText().toString(), 0);
                 toast.show();
+
                 Intent intent = new Intent(getApplicationContext(), MyService.class);
                 intent.putExtra("msg", edit.getText().toString());
                 startService(intent);
