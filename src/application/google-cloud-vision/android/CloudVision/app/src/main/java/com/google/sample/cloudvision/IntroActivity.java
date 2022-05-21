04b32a11921af84852327a422b1e7fd6f88cfd26
Jonggun Park <ooostus@gmail.com>
2022-05-21 22:54:55 +0900

    edit intro activity

--- src/application/google-cloud-vision/android/CloudVision/app/src/main/java/com/google/sample/cloudvision/IntroActivity.java
+++ src/application/google-cloud-vision/android/CloudVision/app/src/main/java/com/google/sample/cloudvision/IntroActivity.java
@@ -2,12 +2,23 @@ package com.google.sample.cloudvision;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class IntroActivity extends AppCompatActivity {
-
+    ImageView logo;
+    TextView title;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        logo = findViewById(R.id.logo);
+        //title = findViewById(R.id.title);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.alpha);
+        logo.startAnimation(animation);
+
     }
 }
